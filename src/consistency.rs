54860dc17f752e7f87c58dd54b5844c52529d688
Nick Kaashoek <nick.kaashoek@gmail.com>
2024-11-17 21:49:45 -0500

    Bunch of refactoring to clean things up and connecting to server

--- src/consistency.rs
+++ src/consistency.rs
@@ -1,11 +1,75 @@
-pub struct check_result {
+// Module for interacting with the consistency check server
+use serde::{Deserialize, Serialize};
+use reqwest::{Client, StatusCode};
+use serde_json::Value;
+
+#[derive(Serialize, Deserialize)]
+pub struct CheckResult {
     pub result: bool,
     pub latency: i64
 }
 
-pub async fn do_consistency_check() -> Result<check_result, ()> {
-    Ok(check_result {
-        result: true,
-        latency: 0
-    })
+pub struct ConsistencyClient {
+    client: Client,
+    url: String
+}
+
+impl ConsistencyClient {
+    pub fn new(url: String) -> Self {
+        Self {
+            client: Client::new(),
+            url: url.clone()
+        }
+    }
+
+    fn ping_endpoint(&self) -> String {
+        format!("{}/", self.url)
+    }
+
+    fn check_endpoint(&self) -> String {
+        format!("{}/check", self.url)
+    }
+
+    fn followup_endpoint(&self) -> String {
+        format!("{}/update", self.url)
+    }
+
+    pub async fn do_ping(&self) -> Result<(), String> {
+        let response = self.client.get(self.ping_endpoint()).send().await.unwrap();
+        match response.status() {
+            StatusCode::OK => Ok(()),
+            _ => Err("Failed to ping".to_string())
+        }
+    }
+
+    pub async fn do_check(&self) -> Result<CheckResult, ()> {
+        let res = self.client.post(self.check_endpoint())
+            .json(&serde_json::json!({}))
+            .send()
+            .await
+            .unwrap();
+
+        let resp_json: Value = res.json().await.unwrap();
+
+        println!("Reponse json: {:?}", resp_json);   
+        Ok(CheckResult {
+            result: true,
+            latency: 0
+        })
+    }
+
+    pub async fn do_followup(&self, writes: Vec<Value>) -> Result<(), ()> {
+        let res = self.client.post(self.followup_endpoint())
+            .json(&serde_json::json!({
+                "writes": writes
+            }))
+            .send()
+            .await
+            .unwrap();
+
+        let resp_json: Value = res.json().await.unwrap();
+
+        println!("Reponse json: {:?}", resp_json);   
+        Ok(())
+    }
 }
---
+++ src/wasm.rs
@@ -0,0 +1,157 @@
+use wasmtime::*;
+use serde_json::Value;
+use serde::{Serialize, Deserialize};
+
+use super::storage::Storage;
+
+#[derive(Debug)]
+pub struct MyState<D: Storage> {
+    external_store: D,
+}
+
+impl <D : Storage> MyState<D> {
+    pub fn reset_writes(&mut self) {
+        self.external_store.reset_writes();
+    }
+    pub fn get_all_writes(&self) -> Vec<Value> {
+        self.external_store.get_all_writes()
+    }
+}
+
+fn read_result_from_wasm<D: Storage>(memory: &Memory, store: &Store<MyState<D>>, offset: usize) -> Result<Vec<u8>> {
+    let mut result_base_bytes = [0; 4];
+    let mut result_len_bytes = [0; 4];
+    memory.read(&store, offset, &mut result_base_bytes)?;
+    memory.read(&store, offset + 4, &mut result_len_bytes)?;
+
+    let result_base = i32::from_le_bytes(result_base_bytes) as usize;
+    let result_len = i32::from_le_bytes(result_len_bytes) as usize;
+    let result_slice = &memory.data(&store)[result_base..][..result_len];
+
+    Ok(result_slice.into())
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct KeySet {
+    pub read_set: Vec<(String, Vec<u8>)>,
+    pub write_set: Vec<(String, Vec<u8>)>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct WasmResult {
+    pub result: Value,
+}
+
+pub struct WasmBlob<D: Storage> {
+    pub store: Store<MyState<D>>,
+    pub module: wasmtime::Module,
+    pub linker: Linker<MyState<D>>,
+}
+
+
+impl<D: Storage> WasmBlob<D> {
+    pub fn setup_blob(config: Config, path: &str, external_store: D) -> Self {
+        let engine = Engine::new(&config).unwrap();
+        let module = wasmtime::Module::from_file(&engine, path).unwrap();
+        let linker = Linker::new(&engine);
+        let state = MyState {
+            external_store: external_store.clone(),
+        };
+        let store = Store::new(&engine, state);
+        Self {
+            store,
+            module,
+            linker,
+        }
+    }
+
+    pub fn link_blob(&mut self) -> wasmtime::Result<()> {
+        self.linker.func_wrap_async("env", "get_key", | mut caller: Caller<'_, MyState<D>>, input: (u32, u32, u32, u32, u32) | {
+            Box::new(async move {
+                let (result_base, table_base, table_len, key_base, key_len) = input;
+                println!("Calling into the get wrapper");
+                let memory = caller.get_export("memory").and_then(|m| m.into_memory()).unwrap();
+                let mut table = Vec::new();
+                table.resize(table_len as usize, 0);
+                memory.read(caller.as_context_mut(), table_base as usize, table.as_mut_slice()).unwrap();
+                let table = String::from_utf8_lossy(&table).to_string();
+
+                let mut key = Vec::new();
+                key.resize(key_len as usize, 0);
+                memory.read(caller.as_context_mut(), key_base as usize, key.as_mut_slice()).unwrap();
+
+                let state = caller.data_mut();
+
+                let result = state.external_store.get(table, &key).await.map(|e| e.1).unwrap_or(Vec::new());
+                let result_offset = memory.data_size(caller.as_context()) - result.len();
+                memory.write(caller.as_context_mut(), result_offset, result.as_slice()).unwrap();
+                memory.write(caller.as_context_mut(), result_base as usize, &((result_offset as u32).to_le_bytes())).unwrap();
+                memory.write(caller.as_context_mut(), result_base as usize + 4, &((result.len() as u32).to_le_bytes())).unwrap();
+
+                println!("reading {:?} {:?}", String::from_utf8(key.clone()), String::from_utf8(result));
+            })
+        })?;
+
+        self.linker.func_wrap_async("env", "put_key", | mut caller: Caller<'_, MyState<D>>, input: (u32, u32, u32, u32, u32, u32) | {
+            Box::new(async move {
+                println!("Calling into the put wrapper");
+                let (table_base, table_len, key_base, key_len, value_base, value_len) = input;
+
+                let memory = caller.get_export("memory").and_then(|m| m.into_memory()).unwrap();
+                let mut table = Vec::new();
+                table.resize(table_len as usize, 0);
+                memory.read(caller.as_context_mut(), table_base as usize, table.as_mut_slice()).unwrap();
+                let table = String::from_utf8_lossy(&table).to_string();
+
+                let mut key = Vec::new();
+                key.resize(key_len as usize, 0);
+                memory.read(caller.as_context_mut(), key_base as usize, key.as_mut_slice()).unwrap();
+
+                let mut value = Vec::new();
+                value.resize(value_len as usize, 0);
+                memory.read(caller.as_context_mut(), value_base as usize, value.as_mut_slice()).unwrap();
+
+                let state = caller.data_mut();
+                println!("writing {:?} {:?}", String::from_utf8(key.clone()), String::from_utf8(value.clone()));
+
+                state.external_store.put(table, key, value).await;
+            })
+        })?;
+        Ok(())
+    }
+
+    pub async fn setup_instance(&mut self, args: Value) -> wasmtime::Result<wasmtime::Instance> {
+        let args_vec = serde_json::to_vec(&args).unwrap();
+        let instance = self.linker.instantiate_async(&mut self.store, &self.module).await?;
+
+        let memory = instance.get_memory(&mut self.store, "memory").unwrap();
+        memory.write(&mut self.store, 8, &args_vec)?;
+        Ok(instance)
+    }
+
+    pub async fn run_blob(&mut self, instance: wasmtime::Instance) -> wasmtime::Result<WasmResult> {
+        // Reset the writes before running the blob
+        self.store.data_mut().reset_writes();
+
+        // Get memory and entry point. Need memory to get the result out
+        let memory = instance.get_memory(&mut self.store, "memory").unwrap();
+        let entry = instance.get_typed_func::<(i32, i32, i32), ()>(&mut self.store, "entry")?;
+        // Can now call the function
+        entry.call_async(&mut self.store, (0, 8, vec!["user-1"].len() as i32)).await?;
+
+        // Fetch and return the result
+        let result_slice = read_result_from_wasm(&memory, &self.store, 0)?;
+        let result_obj = serde_json::from_slice::<WasmResult>(&result_slice).unwrap();
+        Ok(result_obj)
+    }
+
+    pub async fn guess_key(&mut self, instance: wasmtime::Instance) -> wasmtime::Result<KeySet> {
+        let memory = instance.get_memory(&mut self.store, "memory").unwrap();
+        let entry = instance.get_typed_func::<(i32, i32, i32), ()>(&mut self.store, "key_guess")?;
+        entry.call_async(&mut self.store, (0, 8, vec!["user-1"].len() as i32)).await?;
+        let result_slice = read_result_from_wasm(&memory, &self.store, 0)?;
+
+        let result_obj = serde_json::from_slice::<KeySet>(&result_slice).unwrap();
+        Ok(result_obj)
+    }
+}
