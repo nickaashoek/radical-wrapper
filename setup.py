af6e39e8bdaea4e24636cea279253b21444063c1
nickaashoek <nick.kaashoek@gmail.com>
2024-12-05 11:24:09 -0500

    Setup script improvements and requirements.txt

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ import argparse
 import boto3
 from botocore.exceptions import ClientError
 
+RADICAL_TABLE = "radical_testing"
+WRITE_TABLE = "write-intents"
+
 def setup_intent_table(client):
     try:
         client.create_table(
-            TableName='write-intents',
+            TableName=WRITE_TABLE,
             KeySchema=[
                 { "AttributeName": "execution_id", "KeyType": "HASH"}
             ],
@@ -23,7 +26,7 @@ def setup_intent_table(client):
 def setup_item_table(client):
     try:
         client.create_table(
-            TableName='radical-testing',
+            TableName=RADICAL_TABLE,
             KeySchema=[
                 { "AttributeName": "id", "KeyType": "HASH" },
             ],
@@ -40,23 +43,35 @@ def setup_item_table(client):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--region", dest="aws_region", type=str, help="AWS region to setup")
+    parser.add_argument("--user-region", dest="user_region", type=str, help="Near user region to setup")
+    parser.add_argument("--data-region", dest="data_region", type=str, help="Near data region to setup")
     args = parser.parse_args()
     
-    region = args.aws_region
-    print(f"Setting up AWS region: {region}")
+    user_region, data_region = args.user_region, args.data_region
+    print(f"Setting up AWS region pair: {user_region} (user) and {data_region} (data)")
     
-    dynamodb = boto3.client('dynamodb', region_name=region)
+    user_client = boto3.client('dynamodb', region_name=user_region)
+    data_client = boto3.client('dynamodb', region_name=data_region)
     
-    paginator = dynamodb.get_paginator('list_tables')
-    tables = set()
-    for page in paginator.paginate():
+    user_pages = user_client.get_paginator('list_tables')
+    data_pages = data_client.get_paginator('list_tables')
+    user_tables = set()
+    data_tables = set()
+
+    for page in user_pages.paginate():
         for table in page.get('TableNames', []):
-            tables.add(table)
+            user_tables.add(table)
     
-    print("Region {} tables: {}".format(region, tables))
-    if 'write-intents' not in tables:
-        setup_intent_table(dynamodb)
-    if 'radical-testing' not in tables:
-        setup_item_table(dynamodb)
+    for page in data_pages.paginate():
+        for table in page.get('TableNames', []):
+            data_tables.add(table)
+    
+    print("User region {} tables: {}".format(user_region, user_tables))
+    print("Data region {} tables: {}".format(data_region, data_tables))
+    if WRITE_TABLE not in data_tables:
+        setup_intent_table(data_client)
+    if RADICAL_TABLE not in data_tables:
+        setup_item_table(data_client)
+    if RADICAL_TABLE not in user_tables:
+        setup_item_table(user_client)
     
